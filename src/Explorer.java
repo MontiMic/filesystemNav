0031004fc7531277effec3ae18c353412eaed5e5
Michele <michele.monti7@studio.unibo.it>
2023-04-02 11:35:15 +0200

    Multi thread (?), just print the lines

--- src/Explorer.java
+++ src/Explorer.java
@@ -1,5 +1,26 @@
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Objects;
 
 public class Explorer extends Thread {
 
+    public Explorer(File dir){
+        for (File file : Objects.requireNonNull(dir.listFiles())){
+            if (file.isDirectory()) {
+                new Explorer(file);
+            } else {
+                if (file.getName().endsWith(".java")){
+                    try {
+                        System.out.println(file.getName() + "\n" + (int) Files.lines(file.toPath(), StandardCharsets.UTF_8).count() + " lines\n");
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }
+    }
 }
